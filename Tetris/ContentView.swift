101d4c13eb9cf45d002ad16a7141f9989653872c
Dmitry Ryumin <dmitry.ryumin@aviasales.ru>
2024-03-13 11:25:52 +0200

    Initial Commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by glassoff on 13.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by glassoff on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
